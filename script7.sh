fd7d5118001c97075090d2864443c98ec3492811
jsalgua027 <jsalcedoguarde@gmail.com>
2023-11-06 11:11:23 +0100

    iniciado script7

---
+++ script7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+primeraLineaTexto1=$(head -1 texto1.txt)
+primeraLineaTexto2=$(head -1 texto2.txt)
+primeraLineaTexto3=$(head -1 texto3.txt)
+
+if [ "$primeraLineaTexto1" == "alboran"]; then 
+
+
+
+echo $primeraLineaTexto1
+echo $primeraLineaTexto2
+echo $primeraLineaTexto3
